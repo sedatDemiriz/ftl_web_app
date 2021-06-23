4ed1e7dd624929bad5af375bf4d419c1f118605b
sedatDemiriz <sedatdemiriz97@gmail.com>
2021-06-22 17:39:24 -0700

    Re-implemented run viewing API with mixins and generics for brevity

--- view_runs/views.py
+++ view_runs/views.py
@@ -2,94 +2,40 @@ from django.http import Http404
 from rest_framework import viewsets, permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status as s
 from rest_framework.views import APIView
+from rest_framework import status as s
+from rest_framework import generics
+from rest_framework import mixins
 
 from master_db.models import User_Submitted_Run as Run
 from view_runs.serializers import RunSerializer as RS
 
-class RunList(APIView):
+class RunList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
     """
     test
     """
-    def get(self, request, format=None):
-        runs = Run.objects.all()
-        serializer = RS(runs, many=True)
-        return Response(serializer.data)
+    queryset = Run.objects.all()
+    serializer_class = RS
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
     def post(self, request, format=None):
-        serializer = RS(runs, many=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=s.HTTP_201_CREATED)
-        return Response(serializer.errors, status=s.HTTP_400_BAD_REQUEST)
+        return self.create(request, *args, **kwargs)
 
-class RunListDetail(APIView):
+class RunListDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
     """
     test2
     """
-    def get_object(self, pk):
-        try:
-            return Run.objects.get(pk=pk)
-        except Run.DoesNotExist:
-            raise Http404
+    queryset = Run.objects.all()
+    serializer_class = RS
 
-    def get(self, request, pk, format=None):
-        run = self.get_object(pk)
-        serializer = RS(run)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, pk, format=None):
-        run = self.get_object(pk)
-        serializer = RS(runs, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=s.HTTP_400_BAD_REQUEST)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def delete(self, request, pk, format=None):
-        run = self.get_object(pk)
-        run.delete()
-        return Response(s.HTTP_204_NO_CONTENT)    
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
-# @api_view(['GET', 'POST', ])
-# def get_post_runs(request, username, result):
-#     """
-#     test
-#     """
-#     if request.method == 'GET':
-#         runs = Run.objects.all().filter(username=username).filter(result=result)
-#         serializer = RS(runs, many=True)
-#         return Response(serializer.data)
-
-#     elif request.method == 'POST':
-#         serializer = RS(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=s.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=s.HTTP_400_BAD_REQUEST)
-
-# @api_view(['GET', 'PUT', 'DELETE', ])
-# def get_put_del_runs(request, pk):
-#     """
-#     test2
-#     """
-#     try:
-#         runs = Run.objects.get(pk=pk)
-#     except Run.DoesNotExist:
-#         return Response(status=s.HTTP_404_NOT_FOUND)
-
-#     if request.method == 'GET':
-#         serializer = RS(runs)
-#         return Response(serializer.data)
-
-#     elif request.method == 'PUT':
-#         serializer = RS(runs)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=s.HTTP_400_BAD_REQUEST)
-
-#     elif request.method == 'DELETE':
-#         runs.delete()
-#         return Response(status=s.HTTP_204_NO_CONTENT)
