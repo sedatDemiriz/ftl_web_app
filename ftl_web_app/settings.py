ea267b5a655c0e23a9bab2aff87c25b77a5bca93
sedatDemiriz <sedatdemiriz97@gmail.com>
2021-06-24 19:26:12 -0700

    Added CORS headers for localhost API access for development

--- ftl_web_app/settings.py
+++ ftl_web_app/settings.py
@@ -46,9 +46,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -59,6 +61,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'ftl_web_app.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://172.16.42.112:3000',
+]
 
 TEMPLATES = [
     {
