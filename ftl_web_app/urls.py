5fc28c878c66b7c8e21af303064aa1b0ab909864
sedatDemiriz <sedatdemiriz97@gmail.com>
2021-07-04 21:30:13 -0700

    Re-added accidentally removed URL path

--- ftl_web_app/urls.py
+++ ftl_web_app/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     # path('api-runs/<int:pk>/', views.RunListDetail.as_view()),
     path('ann/', include('announcements.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('token-auth/', obtain_jwt_token),
     path('login/', include('login.urls')),
     path('admin/', admin.site.urls),
 ]
